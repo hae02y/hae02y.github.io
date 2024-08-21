7a1f8335c9b78296d76e765a0d373d1645937daa
hae02y <godud1118@gmail.com>
2024-08-22 01:03:30 +0900

    hi

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,255 +1,146 @@
-import type * as Preset from '@docusaurus/preset-classic'
-import type { Config } from '@docusaurus/types'
-import { themes } from 'prism-react-renderer'
-import type { GiscusConfig } from './src/components/Comment'
-
-const user = 'hae02y'
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'hae02y 블로그',
-  url: 'http://hae02y.com',
-  baseUrl: '/',
+  title: 'Hae02y',
+  tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-  organizationName: 'hae02y',
-  projectName: 'blog',
-  customFields: {
-    bio: '道阻且长，行则将至',
-    description:
-      '是一个由愧怍创建的个人博客，主要分享编程开发知识和项目，该网站基于 React 驱动的静态网站生成器 Docusaurus 构建。',
+  // Set the production url of your site here
+  url: 'https://hae02y.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'hae02y', // Usually your GitHub org/user name.
+  projectName: 'hae02y', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
-  themeConfig: {
-    image: 'img/og.png',
-    metadata: [
-      {
-        name: 'author',
-        content: '작성자',
-      },
-      {
-        name: 'keywords',
-        content: 'blog, javascript, typescript, node, react, vue, web',
-      },
-      {
-        name: 'keywords',
-        content: '编程爱好者, Web开发者, 写过爬虫, 学过逆向, 主攻ts全栈',
-      },
-    ],
-    docs: {
-      sidebar: {
-        hideable: true,
-      },
+
+  markdown: {
+      mermaid: true,
     },
-    navbar: {
-      logo: {
-        alt: 'hae02y',
-        src: 'img/logo.webp',
-        srcDark: 'img/logo.webp',
-      },
-      hideOnScroll: true,
-      items: [
-        { label: 'About', position: 'right', to: 'about' },
-        { label: 'Blog', position: 'right', to: 'blog' },
-        { label: 'Project', position: 'right', to: 'project' },
-        { label: 'Friends', position: 'right', to: 'friends' },
-        {
-          label: '더보기',
-          position: 'right',
-          items: [
-            { label: 'Achive', to: 'blog/archive' },
-            { label: 'Skills', to: 'docs/skill' },
-            { label: 'Tools', to: 'docs/tools' },
-          ],
-        },
-        // {
-        //   type: 'localeDropdown',
-        //   position: 'right',
-        // },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `
-        <p>Copyright © 2024 - ${new Date().getFullYear()} hae02y. | Built with Docusaurus.</p>
-        `,
-    },
-    algolia: {
-      appId: 'GV6YN1ODMO',
-      apiKey: '50303937b0e4630bec4a20a14e3b7872',
-      indexName: 'kuizuo',
-    },
-    prism: {
-      theme: themes.oneLight,
-      darkTheme: themes.oneDark,
-      additionalLanguages: ['bash', 'json', 'java', 'python', 'php', 'graphql', 'rust', 'toml', 'protobuf'],
-      defaultLanguage: 'javascript',
-      magicComments: [
-        {
-          className: 'theme-code-block-highlighted-line',
-          line: 'highlight-next-line',
-          block: { start: 'highlight-start', end: 'highlight-end' },
-        },
-        {
-          className: 'code-block-error-line',
-          line: 'This will error',
-        },
-      ],
-    },
-    giscus: {
-      repo: 'kuizuo/blog',
-      repoId: 'MDEwOlJlcG9zaXRvcnkzOTc2MjU2MTI=',
-      category: 'General',
-      categoryId: 'DIC_kwDOF7NJDM4CPK95',
-      theme: 'light',
-      darkTheme: 'dark_dimmed',
-    } satisfies Partial<GiscusConfig>,
-    tableOfContents: {
-      minHeadingLevel: 2,
-      maxHeadingLevel: 4,
-    },
-    liveCodeBlock: { playgroundPosition: 'top' },
-    zoom: {
-      selector: '.markdown :not(em) > img',
-      background: {
-        light: 'rgb(255, 255, 255)',
-        dark: 'rgb(50, 50, 50)',
-      },
-    },
-  } satisfies Preset.ThemeConfig,
+    themes: ['@docusaurus/theme-mermaid'],
+
   presets: [
     [
       'classic',
       {
         docs: {
-          path: 'docs',
-          sidebarPath: 'sidebars.ts',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: false,
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
         theme: {
-          customCss: ['./src/css/custom.css'],
+          customCss: './src/css/custom.css',
         },
-        sitemap: {
-          priority: 0.5,
-        },
-        gtag: {
-          trackingID: 'G-S4SD5NXWXF',
-          anonymizeIP: true,
-        },
-        debug: process.env.NODE_ENV === 'development',
       } satisfies Preset.Options,
     ],
   ],
-  plugins: [
-    'docusaurus-plugin-image-zoom',
-    '@docusaurus/plugin-ideal-image',
-    // ['docusaurus-plugin-baidu-tongji', { token: 'c9a3849aa75f9c4a4e65f846cd1a5155' }],
-    [
-      '@docusaurus/plugin-pwa',
-      {
-        debug: process.env.NODE_ENV === 'development',
-        offlineModeActivationStrategies: ['appInstalled', 'standalone', 'queryString'],
-        pwaHead: [
-          { tagName: 'link', rel: 'icon', href: '/img/logo.png' },
-          { tagName: 'link', rel: 'manifest', href: '/manifest.json' },
-          { tagName: 'meta', name: 'theme-color', content: '#12affa' },
-        ],
-      },
-    ],
-    [
-      'vercel-analytics',
-      {
-        debug: process.env.NODE_ENV === 'development',
-        mode: 'auto',
-      },
-    ],
-    [
-      './src/plugin/plugin-content-blog',
-      {
-        path: 'blog',
-        editUrl: ({ locale, blogDirPath, blogPath, permalink }) =>
-          `https://github.com/kuizuo/blog/edit/main/${blogDirPath}/${blogPath}`,
-        editLocalizedFiles: false,
-        blogDescription: 'hae02y 블로그',
-        blogSidebarCount: 10,
-        blogSidebarTitle: 'Blogs',
-        postsPerPage: 12,
-        showReadingTime: true,
-        readingTime: ({ content, frontMatter, defaultReadingTime }) =>
-          defaultReadingTime({ content, options: { wordsPerMinute: 300 } }),
-        feedOptions: {
-          type: 'all',
-          title: '피드',
-          copyright: `Copyright © ${new Date().getFullYear()} Built with Docusaurus.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${user}</a></p>`,
-        },
-      },
-    ],
-    async function tailwindcssPlugin() {
-      return {
-        name: 'docusaurus-tailwindcss',
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require('tailwindcss'))
-          postcssOptions.plugins.push(require('autoprefixer'))
-          return postcssOptions
-        },
-      }
-    },
-    async function injectMotto() {
-      return {
-        name: 'docusaurus-motto',
-        injectHtmlTags() {
-          return {
-            headTags: [
-              {
-                tagName: 'script',
-                innerHTML: `
-    (${function () {
-      console.log(
-        `%c Kz Blog %c https://github.com/kuizuo/blog`,
-        'color: #fff; margin: 1em 0; padding: 5px 0; background: #12affa;',
-        'margin: 1em 0; padding: 5px 0; background: #efefef;',
-      )
 
-      const motto = `
-This Webisite Powered By Kz Blog.
-Written by Docusaurus, Coding with Love.
---------
-Love what you do and do what you love.
-`
-
-      if (document.firstChild?.nodeType !== Node.COMMENT_NODE) {
-        document.prepend(document.createComment(motto))
-      }
-    }.toString()})();`,
-              },
-            ],
-          }
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
         },
-      }
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/hae02y',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
     },
-  ],
-  headTags: [
-    {
-      tagName: 'meta',
-      attributes: {
-        name: 'description',
-        content: '愧怍的个人博客',
-      },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
-  ],
-  stylesheets: [
-    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Normal.min.css',
-    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Medium.min.css',
-    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Semibold.min.css',
-  ],
-  i18n: {
-    defaultLocale: 'kr',
-    locales: ['en', 'kr'],
-    localeConfigs: {
-      en: {
-        htmlLang: 'en-GB',
-      },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'bash'],
     },
-  },
-  onBrokenLinks: 'warn',
-}
+  } satisfies Preset.ThemeConfig,
+};
 
-export default config
+export default config;
+
