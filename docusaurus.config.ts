3f6e876b5647a50ac016d77b106be7b2ce022b81
hae02y <godud1118@gmail.com>
2024-08-22 17:54:51 +0900

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'HAE02Y',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
