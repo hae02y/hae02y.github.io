2ec3c021c99547e6232dd912c0e3acec546fabe0
hae02y <godud1118@gmail.com>
2025-02-21 18:18:11 +0900

    vault backup: 2025-02-21 18:18:11

--- src/components/MacTerminal/index.tsx
+++ src/components/MacTerminal/index.tsx
@@ -61,7 +61,7 @@ const MacTerminal = ({ title, version }) => {
     }, []);
 
     return (
-        <div className="w-full h-[100%] rounded-lg shadow-md p-4">
+        <div className="w-full h-[100%] rounded-lg p-4">
             <div className="flex items-center mb-4 h-[5%]">
                 <div className="flex gap-2">
                     <div className="bt red w-3 h-3 bg-red-500 rounded-full cursor-pointer"></div>
@@ -72,7 +72,7 @@ const MacTerminal = ({ title, version }) => {
                     My Blog! {version}
                 </div>
             </div>
-            <div className="text-left w-full h-[90%] rounded-md p-4 font-mono text-lg leading-loose bg-gray-200 dark:bg-[#222222]">
+            <div className="text-left w-full h-[90%] rounded-md shadow-lg p-4 font-mono text-lg leading-loose bg-gray-100 dark:bg-[#222222]">
                 <div ref={terminalRef} className="terminal-container w-full h-full" />
             </div>
         </div>
