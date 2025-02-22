c9d0af2d82d21814294358abfff4762d038d194d
hae02y <godud1118@gmail.com>
2025-02-22 13:04:43 +0900

    vault backup: 2025-02-22 13:04:43

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,49 @@ module.exports = {
   theme: {
     mode: 'jit', // JIT 모드 활성화
     extend: {
-      fontFamily: {
-        'custom': ['Pretendard', 'Cera Round Pro', 'sans-serif'],
-      },
       colors: {
-        primary: '#2d3748',
-        secondary: '#718096',
-        accent: '#3182ce',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
   corePlugins: {
     preflight: false,
   },
