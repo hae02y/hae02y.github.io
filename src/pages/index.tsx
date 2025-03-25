6e160b357780fee0c6968b5fdc4a96ebf2e853be
hae02y <godud1118@gmail.com>
2025-03-25 19:40:04 +0900

    vault backup: 2025-03-25 19:40:04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,13 +20,13 @@ function HomepageHeader() {
 // src/components/RecentPosts.tsx
 
 function RecentPosts() {
-    const {blogPosts} = useBlogPost();
+    const blogPost = useBlogPost();
 
     return (
         <div className="w-full max-w-md p-4 space-y-4">
             <h2 className="text-xl font-bold text-white">📌 최근 게시글</h2>
             <ul className="space-y-3">
-                {blogPosts.slice(0, 5).map(({metadata}) => (
+                {blogPost.slice(0, 5).map(({metadata}) => (
                     <li key={metadata.permalink}>
                         <Link
                             to={metadata.permalink}
@@ -62,7 +62,7 @@ function CamelSection() {
                     <div
                         className="h-full w-full lg:h-full lg:w-1/2 flex items-center justify-center bg-white dark:bg-gradient-to-br from-black via-gray-800 to-gray-900"
                     >
-
+                    <RecentPosts/>
 
                     </div>
                 </div>
