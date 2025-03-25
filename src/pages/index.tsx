ebf9567597f20bf623b2619c42af514010ecd7c0
hae02y <godud1118@gmail.com>
2025-03-25 19:50:05 +0900

    vault backup: 2025-03-25 19:50:04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,49 +19,27 @@ function HomepageHeader() {
 
 // src/components/RecentPosts.tsx
 
-function RecentPosts() {
-    const blogPost = useBlogPost();
 
-    return (
-        <div className="w-full max-w-md p-4 space-y-4">
-            <h2 className="text-xl font-bold text-white">📌 최근 게시글</h2>
-            <ul className="space-y-3">
-                {blogPost.slice(0, 5).map(({metadata}) => (
-                    <li key={metadata.permalink}>
-                        <Link
-                            to={metadata.permalink}
-                            className="block text-white hover:underline hover:text-blue-400 transition duration-300"
-                        >
-                            <span className="font-medium">{metadata.title}</span>
-                            <br />
-                            <span className="text-xs text-gray-400">{metadata.formattedDate}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
 
 
 function CamelSection() {
-    const { siteConfig } = useDocusaurusContext();
 
     return (
         <div className="h-[calc(100vh-4rem)] dark:bg-[#23262C] flex">
             <div className="h-full flex w-full flex-col">
-                <div className="lg:flex h-full bg-white dark:bg-gradient-to-br">
+                <div className="lg:flex h-full bg-gradient-to-br from-white via-gray-200 to-gray-300 dark:from-black dark:via-gray-800 dark:to-gray-900">
                     {/* 좌측: 3D Camel */}
                     <div
-                        className="h-full w-full lg:h-full lg:w-1/2 flex items-center justify-center from-black via-gray-800 to-gray-900"
+                        className="h-full w-full lg:h-full lg:w-1/2 flex items-center justify-center"
                     >
                         <TerminalDialog/>
                     </div>
 
                     {/* 우측: 프로필 + 다이얼로그 */}
                     <div
-                        className="h-full w-full lg:h-full lg:w-1/2 flex items-center justify-center from-black via-gray-800 to-gray-900"
+                        className="h-full w-full lg:h-full lg:w-1/2 flex items-center justify-center"
                     >
+
 
                     </div>
                 </div>
