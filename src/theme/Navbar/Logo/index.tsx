6ffc2896e5c7555c955fa9c128fc340f89140590
hae02y <godud1118@gmail.com>
2025-03-28 15:04:56 +0900

    vault backup: 2025-03-28 15:04:55

--- src/theme/Navbar/Logo/index.tsx
+++ src/theme/Navbar/Logo/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import Logo from '@theme/Logo';
+import {Router} from "lucide-react";
 
 export default function NavbarLogo(): JSX.Element {
   return (
-      <button className={'btn'}>
-        <img src="/img/logo/Hae02y-white.svg" alt=""/>
-      </button>
+      <a className="btn" href={'/'}>
+        {/* 라이트 모드용 로고 */}
+        <img
+            src="/img/logo/Hae02y-dark.svg"
+            alt="Logo Light"
+            className="hidden dark:block h-[20px]"
+        />
+
+        {/* 다크 모드용 로고 */}
+        <img
+            src="/img/logo/Hae02y-white.svg"
+            alt="Logo Dark"
+            className="block dark:hidden h-[20px]"
+        />
+      </a>
   );
 }
