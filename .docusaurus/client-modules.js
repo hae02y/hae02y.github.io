021de611f234a00d90a77e087ba9f14391240d4c
hae02y <godud1118@gmail.com>
2025-04-18 14:00:15 +0900

    feat: 변경 사항 커밋

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/vestellalab/Desktop/Study/blog/hae02y.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/vestellalab/Desktop/Study/blog/hae02y.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/vestellalab/Desktop/Study/blog/hae02y.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/vestellalab/Desktop/Study/blog/hae02y.github.io/src/css/custom.css"),
-];
